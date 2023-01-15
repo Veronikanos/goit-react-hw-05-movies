66b7f471fc36c9936910a24c2cb7cf684473b2e0
Veronica <vveronikanika@gmail.com>
2023-01-15 16:54:13 +0200

    feat: show trending movies at home page

--- src/components/service/api.js
+++ src/components/service/api.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
 
-const galleryApiService = axios.create({
-  baseURL: 'https://api.themoviedb.org/',
+const trendingMovies = axios.create({
+  baseURL: 'https://api.themoviedb.org',
   params: {
-    key: '2d95e97f255e7635245c1980eab541d3',
+    api_key: '2d95e97f255e7635245c1980eab541d3',
     // image_type: 'photo',
     // orientation: 'horizontal',
     // per_page: 12,
   },
 });
 
-export const fetchMovie = async (searchQuery, page = 1) => {
+export const fetchTrendingMovies = async (searchQuery, page = 1) => {
   try {
-    const { data } = await galleryApiService.get('', {
-      params: { q: searchQuery, page },
-    });
+    const { data } = await trendingMovies.get('/3/trending/movie/day', {});
+    // console.log(data);
     // const images = data.hits.map(
     //   ({ id, webformatURL, tags, largeImageURL }) => ({
     //     id,
@@ -23,10 +22,8 @@ export const fetchMovie = async (searchQuery, page = 1) => {
     //     largeImageURL,
     //   })
     // );
-    return ();
+    return data.results;
   } catch (error) {
     return error.message;
   }
 };
-
-https://api.themoviedb.org/3/movie/550?api_key=2d95e97f255e7635245c1980eab541d3
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,21 @@
+import { fetchTrendingMovies } from 'components/service/api';
+import { useEffect, useState } from 'react';
+
 export const Home = () => {
-  return <h1>Hey</h1>;
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    fetchTrendingMovies().then(res => setTrendingMovies(res));
+    console.log(trendingMovies);
+  }, []);
+
+  return (
+    <>
+      <h1>Trending today</h1>
+      <ul>
+        {trendingMovies &&
+          trendingMovies.map(movie => <li key={movie.id}>{movie.title}</li>)}
+      </ul>
+    </>
+  );
 };
