e0abbc635da91c6550a1a9ac28fcc5a0966ac723
Veronikanos <49239848+Veronikanos@users.noreply.github.com>
2023-01-18 20:26:47 +0000

    Deploying to gh-pages from @ Veronikanos/goit-react-hw-05-movies@33fb08344edeea0d9a96e5733c302f73a29754de 🚀

---
+++ static/js/239.55d34c79.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[239],{239:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(87),i="Navigation_link__13qe6",c=n(184);var o=function(){return(0,c.jsxs)("nav",{children:[(0,c.jsx)(r.OL,{end:!0,style:function(e){return{color:e.isActive?"#fff":"inherit"}},className:i,to:"/",children:"Home"}),(0,c.jsx)(r.OL,{style:function(e){return{color:e.isActive?"#fff":"inherit"}},className:i,to:"/movies",children:"Movies"})]})}}}]);
+//# sourceMappingURL=239.55d34c79.chunk.js.map
